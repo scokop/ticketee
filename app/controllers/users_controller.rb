2a2a9ed5663f1cc443ad4ff008bc3eb7e54eb232
Scott Kopelchuk <scott.kopelchuk@me.com>
2014-03-22 20:03:03 -0600

    Implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -17,10 +19,28 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+   def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile has been updated."
+
+      redirect_to user_path
+    else
+      flash[:alert] = "Profile has not been updated."
+
+      render action: "edit"
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name,
                                  :password,
                                  :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "bob@gmail.com"
+    password "pencil"
+    password_confirmation "pencil"
+  end
+end
+
