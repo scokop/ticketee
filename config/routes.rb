1ea34c858fabbaf8ea720fcf352b940627ec30ed
Scott Kopelchuk <scott.kopelchuk@me.com>
2014-03-22 20:31:10 -0600

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Ticketee::Application.routes.draw do
     resources :tickets
   end
   resources :users
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
