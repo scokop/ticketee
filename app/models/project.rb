eaf7ee215273f58ec507f54c897f48d5e24329bd
Scott Kopelchuk <scott.kopelchuk@me.com>
2014-10-11 17:58:24 -0600

    Don't show projects that a user doesn't have permission to see

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,14 @@
 class Project < ActiveRecord::Base
-  validates :name, presence: true
   has_many :tickets, dependent: :delete_all
   has_many :permissions, as: :thing
 
+  validates :name, presence: true
+
   scope :viewable_by, ->(user) do 
     joins(:permissions).where(permissions: {action: "view", user_id: user.id })
+  end
+
+  scope :for, ->(user) do
+    user.admin? ? Project.all : Project.viewable_by(user)
   end
 end
--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -13,11 +13,11 @@ module AuthenticationHelpers
     click_button 'Sign in'
     expect(page).to have_content("Signed in successfully.")
   end
+end
 
   def sign_in(user)
     session[:user_id] = user.id
   end
-end
 
 RSpec.configure do |c|
   c.include AuthenticationHelpers
