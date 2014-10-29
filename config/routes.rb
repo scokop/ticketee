0edf890f038d42ff0cf728afa9a03f133145d248
Scott Kopelchuk <scott.kopelchuk@me.com>
2014-10-28 22:30:38 -0600

    Uploaded Assets are now strictly served throuh the FilesController

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Ticketee::Application.routes.draw do
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   delete "/signout", to: "sessions#destroy", as: "signout"
+
+  resources :files
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
